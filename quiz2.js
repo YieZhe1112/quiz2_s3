28781f9b7e3db26130bfac1c7b265ccfbbe4131c
KHOO YIE ZHE <yiezhe1234@gmial.com>
2023-04-04 14:24:34 +0800

    new message

---
+++ quiz2.js
@@ -0,0 +1,20 @@
+let dbUsers=[
+    {   
+        username : "Khoo",
+        password : "password",
+        name : "Khoo",
+        email : "khoo@student.utem.edu.my"
+    },
+    {   
+        username : "Ho",
+        password : "password",
+        name : "Ho",
+        email : "ho@student.utem.edu.my"
+    },
+    {   
+        username : "Tan",
+        password : "password",
+        name : "Tan",
+        email : "tan@student.utem.edu.my"
+    }
+]
