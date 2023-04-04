faf01dc312d4dda6d5dde41af007ae006343997d
KHOO YIE ZHE <yiezhe1234@gmial.com>
2023-04-04 14:56:12 +0800

    update

--- quiz2.js
+++ quiz2.js
@@ -1,20 +1,36 @@
 let dbUsers=[
     {   
         username : "Khoo",
-        password : "password",
+        password : "112233",
         name : "Khoo",
         email : "khoo@student.utem.edu.my"
     },
     {   
         username : "Ho",
-        password : "password",
+        password : "123456",
         name : "Ho",
         email : "ho@student.utem.edu.my"
     },
     {   
         username : "Tan",
-        password : "password",
+        password : "223344",
         name : "Tan",
         email : "tan@student.utem.edu.my"
     }
 ]
+
+function login(reqUsername,reqPassword){
+    let matchUser = dbUsers.find(user => user.username == reqUsername) //=> means array dbUsers will pass every element it find into the variable "user"
+
+    if(!matchUser) return "User not found!"
+
+    if(matchUser.password == reqPassword){
+        return matchUser
+    } 
+    else{
+        return "Invalid password!"
+    }
+}
+
+console.log(login("Khoo","112233")) //try to login
+console.log(login("UTeM","112233")) //try to login
